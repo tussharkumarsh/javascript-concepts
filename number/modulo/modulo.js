414796810f396500469900e2a63603bd6c25f983
Tushar Sharma <tussharkumar.sh@gmail.com>
2021-03-14 16:28:32 +0530

    modulo

---
+++ number/modulo/modulo.js
@@ -0,0 +1,74 @@
+let num1 = 12;
+let num2 = 5;
+
+let ans1 = num1 % num2;
+
+console.log('1. The Answer is: ' + ans1);
+
+/////////////////////////////////////////////////////////
+
+
+let num3 = -12;
+let num4 = 5;
+
+let ans2 = num3 % num4;
+
+console.log('2. The Answer is: ' + ans2);
+
+/////////////////////////////////////////////////////////
+
+
+
+let num5 = 4;
+let num6 = 2;
+
+let ans3 = num5 % num6;
+
+console.log('3. The Answer is: ' + ans3);
+
+/////////////////////////////////////////////////////////
+
+
+
+let num7 = -4;
+let num8 = 2;
+
+let ans4 = num7 % num8;
+
+console.log('4. The Answer is: ' + ans4);
+
+/////////////////////////////////////////////////////////
+
+
+let num9 = 5.5;
+let num10 = 2;
+
+let ans5 = num9 % num10;
+
+console.log('5. The Answer is: ' + ans5);
+
+/////////////////////////////////////////////////////////
+
+
+
+let num11 = NaN;
+let num12 = 2;
+
+let ans6 = num11 % num12;
+
+console.log('6. The Answer is: ' + ans6);
+
+/////////////////////////////////////////////////////////
+
+
+
+let num13 = Infinity;
+let num14 = 2;
+
+let ans7 = num13 % num14;
+
+console.log('7. The Answer is: ' + ans7);
+
+/////////////////////////////////////////////////////////
+
+
