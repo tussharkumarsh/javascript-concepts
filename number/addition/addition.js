875064046221215d1dff4c3f2b69f0ee149b30f9
Tushar Sharma <tussharkumar.sh@gmail.com>
2021-03-14 14:32:11 +0530

    addition 10 example

--- number/addition/addition.js
+++ number/addition/addition.js
@@ -1,7 +1,124 @@
+// 1. Simple Addition two numbers
+
 let num1 = 5;
 let num2 = 3;
 
-let total = num1 + num2;
+let ans1 = num1 + num2;
+
+console.log('The total is: ' + ans1);
 
 
-console.log('The total is: ' + total);
+/////////////////////////////////////////////////////////
+
+// 2. Addition with negative number
+
+let num3 = 10;
+let num4 = -3;
+
+let ans2 = num3 + num4;
+
+console.log('The total is: ' + ans2);
+
+
+/////////////////////////////////////////////////////////
+
+// 3. Addition with multiply
+
+let num5 = 10;
+let num6 = 3;
+let num7 = 7;
+
+let ans3 = num5 + (num6 * num7);
+
+console.log('The total is: ' + ans3);
+
+
+/////////////////////////////////////////////////////////
+
+// 4. Addition with divide
+
+let num8 = 4;
+let num9 = 3;
+let num10 = 3;
+
+let ans4 = (num8 / num9) + num10;
+
+console.log('The total is: ' + ans4);
+
+
+/////////////////////////////////////////////////////////
+
+// 5. Addition with one string
+
+let num11 = "4";
+let num12 = 3;
+
+let ans5 = num11 + num12;
+
+console.log('The total is: ' + ans5);
+
+
+/////////////////////////////////////////////////////////
+
+// 6. Addition with one string
+
+let num13 = '4';
+let num14 = 5;
+
+let ans6 = Number(num13) + num14;
+
+console.log('The total is: ' + ans6);
+
+
+/////////////////////////////////////////////////////////
+
+// 7. Addition with both string
+
+let num15 = '4';
+let num16 = '5';
+
+let ans7 = Number(num15) + num16;
+
+console.log('The total is: ' + ans7);
+
+
+/////////////////////////////////////////////////////////
+
+// 8. Addition with decimal number
+
+let num17 = 33.252;
+let num18 = 41.1122;
+let num19 = 32
+
+let ans8 = num17 + (num18/num19);
+
+console.log('The total is: ' + ans8);
+
+
+
+
+/////////////////////////////////////////////////////////
+
+// 9. Addition with one string in stored veriable
+
+let num20 = 45.25;
+let num21 = 47.75;
+let num22 = 7
+
+let ans9 =  '7' + num20 + num21  + num22;
+
+console.log('Answer is ' + ans9);
+
+
+
+/////////////////////////////////////////////////////////
+
+// 10. Addition with both string
+
+let num23 = 45.25;
+let num24 = 47.75;
+let num25 = 7.12
+
+let ans10 =  + num23 + num24  + num25;
+
+console.log('Answer is ' + (ans10));
