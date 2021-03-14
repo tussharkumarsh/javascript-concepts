10d1aba2bf72b217e5071a5d88a527381174f426
Tushar Sharma <tussharkumar.sh@gmail.com>
2021-03-14 15:34:41 +0530

    division

---
+++ number/division/division.js
@@ -0,0 +1,47 @@
+let num1 = 12;
+let num2 = 2;
+
+let ans1 = num1 / num2;
+
+console.log('1. The Answer is: ' + ans1);
+
+/////////////////////////////////////////////////////////
+
+
+let num3 = 5;
+let num4 = 2;
+
+let ans2 = num3 / num4;
+
+console.log('2. The Answer is: ' + ans2);
+
+/////////////////////////////////////////////////////////
+
+
+let num5 = '12';
+let num6 = 6;
+
+let ans3 = num5 / num6;
+
+console.log('3. The Answer is: ' + ans3);
+
+/////////////////////////////////////////////////////////
+
+
+let num7 = 2;
+let num8 = 0;
+
+let ans4 = num7 / num8;
+
+console.log('4. The Answer is: ' + ans4);
+
+/////////////////////////////////////////////////////////
+
+let num9 = 0;
+let num10 = 2;
+
+let ans5 = num9 / num10;
+
+console.log('5. The Answer is: ' + ans5);
+
+/////////////////////////////////////////////////////////
