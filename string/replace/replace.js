47599bf766f992c58c6c98d70eac848dc0a01e75
Tushar Sharma <tussharkumar.sh@gmail.com>
2021-03-17 08:28:54 +0530

    string

---
+++ string/replace/replace.js
@@ -0,0 +1,8 @@
+let str = "Welcome to Jaipur";
+console.log(str.replace("Jaipur","Ahmedabad"));
+
+
+const p = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
+
+console.log(p.replace('dog', 'monkey'));
+// expected output: "The quick brown fox jumps over the lazy monkey. If the dog reacted, was it really lazy?"
